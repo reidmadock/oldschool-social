cb610afa79d0a7e494960a595fc232e2ec7365ff
Reid M <reidmadock@gmail.com>
2023-06-25 15:35:06 -0500

    route skeletons added

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,34 @@
 const router = require('express').Router();
 
+router.get('/', async (req, res) => {
+
+});
+
+router.post('/', async (req, res) => {
+
+});
+
+/**
+* TODO: get single thought, update user, delete user.
+*/
+router.get('/:thoughtId', async (req, res) => {
+
+});
+router.get('/:thoughtId', async (req, res) => {
+
+});
+router.get('/:thoughtId', async (req, res) => {
+
+});
+
+/**
+ * TODO: get single reaction, delete reaction.
+ */
+router.get('/:thoughtId/reactions/:reactionId', async (req, res) => {
+
+});
+router.get('/:thoughtId/reactions/:reactionId', async (req, res) => {
+
+});
+
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -11,22 +11,23 @@ router.post('/', async (req, res) => {
 /**
 * TODO: get single user, update user, delete user.
 */
-router.get('/:userid', async (req, res) => {
+router.get('/:userId', async (req, res) => {
 
 });
-router.get('/:userid', async (req, res) => {
+router.get('/:userId', async (req, res) => {
 
 });
-router.get('/:userid', async (req, res) => {
+router.get('/:userId', async (req, res) => {
 
 });
+
 /**
  * TODO: get single friend, delete friend.
  */
-router.get('/:userid/friends/:friendid', async (req, res) => {
+router.get('/:userId/friends/:friendId', async (req, res) => {
 
 });
-router.get('/:userid/friends/:friendid', async (req, res) => {
+router.get('/:userId/friends/:friendId', async (req, res) => {
 
 });
 
